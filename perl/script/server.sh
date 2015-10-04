6ce0b8aa550d3519167e7a2edf02cf7af00ce927
ichirin2501 <ichirin2501@hatena.ne.jp>
2015-10-05 00:40:51 +0900

    fix server-starter opt

--- perl/script/server.sh
+++ perl/script/server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-exec /home/isucon/.local/perl/bin/carton exec start_server -- plackup -s Starlet -p 8080 --workers 4 --disable-keepalive app.psgi
+exec /home/isucon/.local/perl/bin/carton exec start_server --port 8080 -- \
+    plackup -s Starlet --workers 4 --max-reqs-per-child 10000 --disable-keepalive app.psgi
