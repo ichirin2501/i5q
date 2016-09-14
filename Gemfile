615e64d7893d4b7a177ba2bb7444153c284510ec
ichirin2501 <ichirin2501@hatena.ne.jp>
2016-09-14 21:20:33 +0900

    一旦全部消す

--- Gemfile
+++
@@ -1,4 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'capistrano', '2.15.5'
-gem 'capistrano_colors', '0.5.5'
--- perl/script/server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-exec /home/isucon/.local/perl/bin/carton exec start_server --port 8080 -- \
-    plackup -s Starlet --workers 4 --max-reqs-per-child 10000 --disable-keepalive app.psgi
