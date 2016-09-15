49246e5e513153fdfb3492e1526a9aa1bbba752e
ichirin2501 <ichirin2501@hatena.ne.jp>
2016-09-16 03:04:06 +0900

    カラム追加して無駄なjoinをやめる

--- lib/Isucon5/Web.pm
+++ lib/Isucon5/Web.pm
@@ -209,12 +209,7 @@ get '/' => [qw(set_global authenticated)] => sub {
     my $entries = db->select_all($entries_query, current_user()->{id});
 
     my $comments_for_me_query = <<SQL;
-SELECT c.id AS id, c.entry_id AS entry_id, c.user_id AS user_id, c.comment AS comment, c.created_at AS created_at
-FROM comments c
-JOIN entries e ON c.entry_id = e.id
-WHERE e.user_id = ?
-ORDER BY c.created_at DESC
-LIMIT 10
+SELECT * FROM comments FROM owner_id = ? ORDER BY created_at DESC LIMIT 10
 SQL
     my $comments_for_me = [];
     my $comments = [];
@@ -416,9 +411,9 @@ post '/diary/comment/:entry_id' => [qw(set_global authenticated)] => sub {
     if ($entry->{is_private} && !permitted($entry->{user_id})) {
         abort_permission_denied();
     }
-    my $query = 'INSERT INTO comments (entry_id, user_id, comment) VALUES (?,?,?)';
+    my $query = 'INSERT INTO comments (entry_id, user_id, comment, owner_id) VALUES (?,?,?,?)';
     my $comment = $c->req->param('comment');
-    db->query($query, $entry->{id}, current_user()->{id}, $comment);
+    db->query($query, $entry->{id}, current_user()->{id}, $comment, $entry->{user_id});
     redirect('/diary/entry/'.$entry->{id});
 };
 
